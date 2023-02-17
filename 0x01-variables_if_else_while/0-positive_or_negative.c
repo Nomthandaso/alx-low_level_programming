3844a53fca308a3fe7deb1fbebf937a2afa59586
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-17 14:46:29 -0800

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ * It returns the same sequence of random number
+    on every execution of the program.
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf(" Random Numbers are: \n")
+	return (0);
+}
