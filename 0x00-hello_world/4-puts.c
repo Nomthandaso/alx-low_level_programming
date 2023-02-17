67c792fc21538da353eb6d0b24af86139dba679b
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-16 23:31:17 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * return: always 0, (success)
+ * Main: Entry point
+ * Return: always 0, (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
