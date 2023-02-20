e4d73f81b03ffafd2b28289326defe4b1cbdc6c4
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-20 12:53:06 -0800

    base 10 via putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print single numbers of base 10 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar (i);
+	{
+		putchar ('\n');
+	}
+	return (0);
+}
