130d8a8a1e723db1945ebb86d98d8063f2334a72
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-20 06:57:26 -0800

    Dreary

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * function main
+ * main
+ *
+ * Description: 'putchar the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ch;
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-		putchar(ch);
+		putchar ("ch\n");
 	return(0);
 }
