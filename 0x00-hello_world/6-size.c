cfe9c3eb6dc1a612fdd3a32df2d7834e51bf0a74
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-21 03:42:09 -0800

    format

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,18 +8,11 @@
  */
 int main(void)
 {
-    int integerType;
-
-    float floatType;
-
-    double doubleType;
-
-    char charType;
-
-    printf("Size of int: %ld bytes\n",sizeof(integerType));
-    printf("Size of float: %ld bytes\n",sizeof(floatType));
-    printf("Size of double: %ld bytes\n",sizeof(doubleType));
-    printf("Size of char: %ld byte\n",sizeof(charType));
+    printf("size of char is = %i byte\n", sizeof(char));
+    printf("size of int is = %i byte\n", sizeof(int));
+    printf("size of long int is = %i byte\n", sizeof(long int));
+    printf("size of long long int is = %i byte\n", sizeof(long long int));
+    printf("size of float is = %i byte\n", sizeof(float));
 
     return (0);
 }
