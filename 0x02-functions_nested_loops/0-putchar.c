38b2351c2fbde417817f9fb0444d7b6c1b9ca035
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-21 13:47:02 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar ("_putchar\n");
+	return (0);
+}
