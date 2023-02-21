0acb237077273699ce11e853c1cbc5a7273c7534
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-20 21:43:15 -0800

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,9 +15,9 @@ int main(void)
 	for (n = 0 ; n <= 9 ; n++)
 		putchar ('0' + n);
 	if (n > 0)
-		putchar (", ");
+		putchar (', ');
 	if (n < 10)
-		putchar (", ");
+		putchar (', ');
 	{
 		putchar ('\n');
 	}
