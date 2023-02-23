8b8ebd9c28eab0c7e868fb88ffa69a638c886ae9
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-22 21:16:04 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * Description: computes the absolute value of an integer
+ *
+ * @c: nimber to be computed
+ *
+ * Return: Absolute value of number or 0
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
