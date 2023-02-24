16fe41ff2223cb5cdd0dfc1f7d1d70a0676b6e22
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-24 05:46:26 -0800

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
         int i;
 
-        printf("Infinite loop incoming :(\n");
+        printf("Infinite loop incoming :(\n"); /** causes output into an infinite loop */
 
         i = 0;
 
-        while (i < 10)	/** causes output into an infinite loop */
+        while (i < 10)
         {
-                putchar(i);	/** causes output into an infinite loop */
+                putchar(i);
         }
 
         printf("Infinite loop avoided! \\o/\n");
