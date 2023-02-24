fc6a411d04bb5989c6b58045d6c3ee26757d1a49
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-24 12:34:24 -0800

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,7 +10,10 @@ int main(void)
         int i;
 
         i = 0;
-        positive_or_negative(i);
+        void positive_or_negative(int i);
+	int largest_number(int a, int b, int c);
+	int convert_day(int month, int day);
+	void print_remaining_days(int month, int day, int year);
 
         return (0);
 }
