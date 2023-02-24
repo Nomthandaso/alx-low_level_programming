c3e53b588823148aa319abedceb472b5179483fc
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-24 02:50:13 -0800

    integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: adds two integers and returns the result
+ *
+ * @n1: first number to be computed
+ *
+ * @n2: second number to be computed
+ *
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
