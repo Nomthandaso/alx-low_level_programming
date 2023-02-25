5592c8a36f20fad628ccca81b19fd299553ad6a6
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-25 10:52:11 -0800

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,22 @@
 #include "main.h"
+#include <stdio>
+#include <stdlib>
 /**
  * print_numbers - Entry point
  *
  * Description: prints the numbers, from 0 to 9
  *
- * Return: void
+ * @i: number being printed
+ *
+ * Return: numbers from 0 to 9
  */
 void print_numbers(void)
 {
-	int _putchar(int i);
-
 	int i;
 	
 	for (i = 0 ; i <= 9 ; i++)
-	_putchar (i + '0');
 	{
-		_putchar ('\n');
+		_putchar (i + '0');
 	}
+		_putchar ('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int _putchar(int i);
 void positive_or_negative(int i);
 int _isupper(int c);
 int _isdigit(int c);
