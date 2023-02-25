e55685c0f07b76f0c9a1b3c93fda61ac71bd625a
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-25 03:15:25 -0800

    comiit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - Entry point
+ *
+ * Description: prints a triangle
+ *
+ * @size: The size of the squre triangle
+ *
+ * Return: empty
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0 ; x < size ; x++)
+		{
+			for (y = size - x ; y > 1 ; y--)
+			{
+				_putchar(32);
+			}
+			for (2 = 0 ; 2 <= ; 2++ xxx
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+void positive_or_negative(int i);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
