10848fdd8634e2c24b1c74ab91ff25c28079556f
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-02-26 10:06:03 -0800

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,26 +2,31 @@
 /**
  * print_triangle - Entry point
  *
- * Description: prints a triangle
+ * Description: prints a triangle, followed by a new line
  *
- * @size: The size of the squre triangle
- *
- * Return: empty
+ * @size: size of the triangle
  */
 void print_triangle(int size)
 {
-	int x, y, z;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (x = 0 ; x < size ; x++)
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			for (y = size - x ; y > 1 ; y--)
+			for (j = i; j < size; j++)
 			{
-				_putchar(32);
+				_putchar(' ');
 			}
-			for (2 = 0 ; 2 <= ; 2++ xxx
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
