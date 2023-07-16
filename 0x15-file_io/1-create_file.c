5742a22e34cda8fb4e564aaee20c1790b4558401
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-07-16 10:25:39 -0700

    later than ever

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for(i = 0; text_content[i];)
+		for (i = 0; text_content[i];)
 			i++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wt = write(fd, text_content, i);
 	if (fd == -1 || wt == -1)
 		return (-1);
